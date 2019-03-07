2468e6985bb4a3d1604820710afcb014a176ea14
Israel Gelover <i.gelover@synergysportstech.com>
2019-03-06 21:16:55 -0600

    Added initial API and Tests project templates

---
+++ QuickDemo.API/QuickDemo.API/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace QuickDemo.API.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
